a92bd0eb4cc0ac7b1e878ef3ec91fe1c56d64867
amine troudi <troudi.2015@hotmail.com>
2017-04-03 22:00:08 +0100

    updating year process | parsing xl |

--- marsad_budget/budget.py
+++ marsad_budget/budget.py
@@ -16,7 +16,75 @@ def unit_of_work(spreedsheet):
             print row[0].value
             tmp_dict["ar"] = row[0].value
             tmp_dict["fr"] = row[1].value
-            tmp_dict["budget"] = row[2].value
+            tmp_dict["budget"] = int(row[2].value)
+            #gestion
+            if (row[3].value) :
+                 tmp_dict["remunerations_publique"] =  int(row[3].value)
+            else:
+                 tmp_dict["remunerations_publique"] =  0
+
+
+            if (row[4].value) :
+                 tmp_dict["moyens_des_services"] =  int(row[4].value)
+            else:
+                 tmp_dict["moyens_des_services"] =  0
+
+            if (row[5].value) :
+                 tmp_dict["interventions_publiques"] =  int(row[5].value)
+            else:
+                 tmp_dict["interventions_publiques"] =  0
+
+            if (row[6].value) :
+                 tmp_dict["disposition_urgence"] =  int(row[6].value)
+            else:
+                 tmp_dict["disposition_urgence"] =  0
+
+
+            if (row[7].value) :
+                 tmp_dict["Avantages_dette_publique"] =  int(row[7].value)
+            else:
+                 tmp_dict["Avantages_dette_publique"] =  0
+
+
+            if (row[8].value) :
+                 tmp_dict["gestion"] =  int(row[8].value)
+            else:
+                 tmp_dict["gestion"] =  0
+
+
+            if (row[9].value) :
+                 tmp_dict["Investissements_direct"] =  int(row[9].value)
+            else:
+                 tmp_dict["Investissements_direct"] =  0
+
+
+            if (row[10].value) :
+                 tmp_dict["financement_public"] =  int(row[10].value)
+            else:
+                 tmp_dict["financement_public"] =  0
+
+
+            if (row[11].value) :
+                 tmp_dict["Depenses_developpement_urgence"] =  int(row[11].value)
+            else:
+                 tmp_dict["Depenses_developpement_urgence"] =  0
+
+
+            if (row[12].value) :
+                 tmp_dict["ressources_exterieures_affectees"] =  int(row[12].value)
+            else:
+                 tmp_dict["ressources_exterieures_affectees"] =  0
+
+
+            if (row[13].value) :
+                 tmp_dict["Remboursement_dette_publique"] =  int(row[13].value)
+            else:
+                 tmp_dict["Remboursement_dette_publique"] =  0
+            if (row[14].value) :
+                 tmp_dict["developpement"] =  int(row[14].value)
+            else:
+                 tmp_dict["developpement"] =  0
+
             To_return.append(tmp_dict)
             tmp_dict = {}
     return To_return
